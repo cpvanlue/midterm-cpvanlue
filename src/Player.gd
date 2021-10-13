d24c82a2af36d2412ace9d3f35f0ea1012f87e27
Caleb Van Lue <cpvanlue@bsu.edu>
2021-10-13 00:05:42 -0400

    Fix gravity and movement mechanics

--- src/Player.gd
+++ src/Player.gd
@@ -10,7 +10,8 @@ var velocity := Vector2(0, 0)
 
 func _physics_process(delta: float) -> void:
 	reset_gravity_on_floor()
-	set_animation_and_speed(delta)
+	set_direction()
+	check_abilities(delta)
 	if is_on_ceiling():
 		velocity.y = 100
 	var _ignored = move_and_slide(velocity, Vector2(0, -1))
@@ -23,20 +24,27 @@ func _physics_process(delta: float) -> void:
 
 func reset_gravity_on_floor() -> void:
 	if is_on_floor():
-		velocity.y = 1000
+		velocity.y = 980
 
 
-func set_animation_and_speed(delta: float) -> void:
+func set_direction() -> void:
 	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x = -SPEED * 1.5
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("walk")
 	elif Input.is_action_pressed("right"):
 		velocity.x = SPEED
+		if Input.is_action_pressed("sprint"):
+			velocity.x = SPEED * 1.5
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("walk")
 	else:
 		$AnimatedSprite.play("idle")
+
+
+func check_abilities(delta: float) -> void:
 	if Input.is_action_pressed("crouch") and is_on_floor():
 		$AnimatedSprite.play("duck")
 		$Hitbox.scale.y = 0.3
@@ -50,7 +58,5 @@ func set_animation_and_speed(delta: float) -> void:
 				velocity.y += jump_impulse * 1.15
 		else:
 			velocity.y += jump_impulse
-	if Input.is_action_pressed("sprint") and is_on_floor():
-		velocity.x = velocity.x * 1.7
 	else:
 		velocity.y += GRAVITY * 3 * delta
