66a224debee485918c0145c21895e8e1bbb9f6b4
Caleb Van Lue <cpvanlue@bsu.edu>
2021-10-14 01:18:16 -0400

    Fix misplaced scene and script in directory

--- src/Level.gd
+++ src/Level.gd
@@ -42,7 +42,7 @@ func game_Timer() -> void:
 
 
 func _on_Replay() -> void:
-	var _ignored = get_tree().change_scene("res://MainMenu.tscn")
+	var _ignored = get_tree().change_scene("res://src/MainMenu.tscn")
 
 
 func _on_Coin_Get() -> void:
