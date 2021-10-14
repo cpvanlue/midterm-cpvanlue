632b45199b6f7455f48ac1b815ff2ebb624a5001
Caleb Van Lue <cpvanlue@bsu.edu>
2021-10-14 03:47:06 -0400

    Adjust sound volume and fix error

--- src/Coin.gd
+++ src/Coin.gd
@@ -4,7 +4,7 @@ extends Area2D
 signal coin_get
 
 
-func _on_Coin_body_entered(body: RigidBody2D) -> void:
+func _on_Coin_body_entered(body: PhysicsBody2D) -> void:
 	if body.name == "Player":
 		emit_signal("coin_get")
 		queue_free()
