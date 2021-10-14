c909b9d702b22034642c0992a1714cbdb2ed3f10
Caleb Van Lue <cpvanlue@bsu.edu>
2021-10-14 01:42:52 -0400

    Add one-way tile passthrough to tiles at end of level

--- src/MainMenu.gd
+++ src/MainMenu.gd
@@ -6,5 +6,10 @@ func _ready() -> void:
 	$AnimationPlayer.play("Load")
 
 
+func _process(_delta):
+	if Input.is_action_pressed("ui_accept"):
+		_on_Button_pressed()
+
+
 func _on_Button_pressed() -> void:
 	var _ignored = get_tree().change_scene("res://src/Level.tscn")
